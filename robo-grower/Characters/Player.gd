235fa3b3fa8a24de682189153b3d13e3c1d82c48
Kyle <kbburkholder@gmail.com>
2021-10-13 22:01:28 -0400

    Started adding light

--- robo-grower/Characters/Player.gd
+++ robo-grower/Characters/Player.gd
@@ -1,27 +1,35 @@
 extends KinematicBody2D
 
-var speed = 250
-var velocity = Vector2()
+var speed = 500
+var gravity = 32
+var jumpforce = -1000
+var velocity = Vector2.ZERO
 
 func get_input():
 	# Detect up/down/left/right keystate and only move when pressed.
-	velocity = Vector2()
 	if Input.is_action_pressed('ui_right'):
-		velocity.x += 1
-	if Input.is_action_pressed('ui_left'):
-		velocity.x -= 1
-	if Input.is_action_pressed('ui_down'):
-		velocity.y += 1
-	if Input.is_action_pressed('ui_up'):
-		velocity.y -= 1
-	velocity = velocity.normalized() * speed
-	animate(velocity)
+		velocity.x = speed
+	elif Input.is_action_pressed('ui_left'):
+		velocity.x = -speed
+	else:
+		velocity.x = lerp(velocity.x, 0, 0.25)
+
+	if Input.is_action_pressed('jump') and is_on_floor():
+		velocity.y = jumpforce
+	
+	
 
 func _physics_process(delta):
 	get_input()
-	move_and_collide(velocity * delta)
+	animate()
+	apply_gravity(delta)
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
 
-func animate(velocity):
+func apply_gravity(delta):
+	velocity.y += gravity + delta
+
+func animate():
 	if $AnimatedSprite.is_playing() and velocity == Vector2():
 		$AnimatedSprite.stop()
 	elif not $AnimatedSprite.is_playing():
